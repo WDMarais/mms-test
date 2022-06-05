d6319a58b69f21a7820c899ad2757cdb32fccc91
Wynand Marais <marais.wynand@gmail.com>
2022-06-05 13:40:44 +0200

    Add basic function structure and unit test framework

--- algorithm.py
+++ algorithm.py
@@ -1,2 +1,15 @@
+import unittest
+
+def f(a, x):
+    N = len(a)
+    if N == 0:
+        return -1
+
+class TestFunction(unittest.TestCase):
+    def test_empty_array(self):
+        actual = f([], 1)
+        expected = -1
+        self.assertEqual(actual, expected)
+
 if __name__ == '__main__':
-    print('Hello world!')
+    unittest.main()
