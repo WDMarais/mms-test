1ac26825b025727d22f3524291a93b99ed71b53b
Wynand Marais <marais.wynand@gmail.com>
2022-06-05 13:51:22 +0200

    Slight refactoring

--- algorithm.py
+++ algorithm.py
@@ -1,18 +1,26 @@
 import unittest
 
-def is_sorted(l):
-    return all(a <= b for a, b in zip(l, l[1:]))
-
-def f(a, x):
-    N = len(a)
+def params_are_valid(a, x, N):
     # If array is empty, it can't contain x or any smaller elements
     if N == 0:
-        return -1
+        return False
 
     # Design choice - spec stated list was sorted, algorithm uses this fact, so if
     # the list is not sorted, also return error. Alternatively, list could be sorted, but that
     # would add n log n processing time, whereas the check and error adds n processing time in the worst case.
     if not is_sorted(a):
+        return False
+    return True
+
+def is_sorted(l):
+    return all(a <= b for a, b in zip(l, l[1:]))
+
+def f(a, x):
+    N = len(a)
+    # Check basic stuff, like
+    # - Whether the list contains elements at all
+    # - Whether the list is sorted
+    if not params_are_valid(a, x, N):
         return -1
 
 class TestFunction(unittest.TestCase):
