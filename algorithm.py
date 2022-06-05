3776580bcd1fb81706a1c3315eaf2af713d8897b
Wynand Marais <marais.wynand@gmail.com>
2022-06-05 13:48:39 +0200

    Add check for sorted arrays

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,28 @@
 import unittest
 
+def is_sorted(l):
+    return all(a <= b for a, b in zip(l, l[1:]))
+
 def f(a, x):
     N = len(a)
+    # If array is empty, it can't contain x or any smaller elements
     if N == 0:
+        return -1
+
+    # Design choice - spec stated list was sorted, algorithm uses this fact, so if
+    # the list is not sorted, also return error. Alternatively, list could be sorted, but that
+    # would add n log n processing time, whereas the check and error adds n processing time in the worst case.
+    if not is_sorted(a):
         return -1
 
 class TestFunction(unittest.TestCase):
     def test_empty_array(self):
         actual = f([], 1)
+        expected = -1
+        self.assertEqual(actual, expected)
+
+    def test_unsorted_array(self):
+        actual = f([1, 3, 2], 1)
         expected = -1
         self.assertEqual(actual, expected)
 
